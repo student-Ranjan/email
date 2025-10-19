4667b29ad21462364251b10db72e856eaa280e01
student-Ranjan <153839561+student-Ranjan@users.noreply.github.com>
2025-10-19 11:21:42 +0530

    Collected static files for deployment

--- Myproject/settings.py
+++ Myproject/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 STATIC_DIR=BASE_DIR/'static'
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS=[STATIC_DIR]
+STATICFILES_DIRS=[BASE_DIR / 'static']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
